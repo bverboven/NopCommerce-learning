8d3d271f26b1c4b3ff7fc521bc312ad36faf70d2
bverboven <bramverboven@hotmail.com>
2026-01-22 08:24:11 +1100

    cache event

--- src/Libraries/Nop.Services/SupportRequests/EventConsumer.cs
+++ src/Libraries/Nop.Services/SupportRequests/EventConsumer.cs
@@ -1,16 +1,18 @@
 ﻿using Nop.Core.Domain.Localization;
 using Nop.Core.Domain.SupportRequests;
 using Nop.Core.Events;
+using Nop.Services.Caching;
 using Nop.Services.Events;
 using Nop.Services.Messages;
 
 namespace Nop.Services.SupportRequests;
 
 public class EventConsumer(IWorkflowMessageService workflowMessageService, LocalizationSettings localizationSettings)
-    : IConsumer<EntityInsertedEvent<SupportRequest>>
+    : CacheEventConsumer<SupportRequest>, IConsumer<EntityInsertedEvent<SupportRequest>>
 {
-    public async Task HandleEventAsync(EntityInsertedEvent<SupportRequest> eventMessage)
+    public override async Task HandleEventAsync(EntityInsertedEvent<SupportRequest> eventMessage)
     {
+        await base.HandleEventAsync(eventMessage);
         await workflowMessageService.SendSupportRequestStoreOwnerNotificationMessageAsync(eventMessage.Entity, localizationSettings.DefaultAdminLanguageId);
     }
 }
